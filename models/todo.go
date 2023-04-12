fb6620617497da99edc26417bd5f080175676666
Prinata Rakha Santoso <prinatarakha@gmail.com>
2023-04-12 22:50:35 +0700

    Buat model dan dto response nya

--- models/todo.go
+++ models/todo.go
@@ -2,6 +2,10 @@ package models
 
 import "gorm.io/gorm"
 
+// https://jsonplaceholder.typicode.com/todos
 type Todo struct {
 	gorm.Model
+	UserID    uint   `json:"userId"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
 }
